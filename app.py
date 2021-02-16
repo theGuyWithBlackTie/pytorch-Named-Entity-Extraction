0698af9be4fd5a431490ab8cbe9cf5ec608c0f30
theguy-with-blacktie <ashish.ash216@gmail.com>
2021-02-16 05:51:53 +0100

    Initial Commit

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from sklearn import preprocessing
 from sklearn import model_selection
 from transformers import AdamW
 from transformers import get_linear_schedule_with_warmup
+import joblib
 
 import config
 import dataset
@@ -20,6 +21,38 @@ def run():
         "enc_pos": enc_pos,
         "enc_tag": enc_tag
     }
+
+    joblib.dump(meta_data, "meta.bin")
+
+    num_pos = len(list(enc_pos.classes_))
+    num_tag = len(list(enc_tag.classes_))
+
+    (
+        train_sentences,
+        test_sentences,
+        train_pos,
+        test_pos,
+        train_tag,
+        test_tag
+    ) = model_selection.train_test_split(sentences, pos, tag, random_state=42, test_size=0.1)
+
+    train_dataset = dataset.EntityDataset(
+        texts = train_sentences, pos=train_pos, tags=train_tag
+    )
+
+    test_dataset  = dataset.EntityDataset(
+        texts = test_sentences, pos=test_pos, tags=test_tag
+    )
+
+    train_data_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size = config.TRAIN_BATCH_SIZE, num_workers=4
+    )
+
+    test_data_loader  = torch.utils.data.DataLoader(
+        test_dataset, batch_size=config.VALID_BATCH_SIZE, num_workers=1
+    )
+
+    device = torch.device("cuda")
 
 if __name__ == "__main__":
 
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 MAX_LEN          = 128
 TRAIN_BATCH_SIZE = 32
 VALID_BATCH_SIZE = 8
